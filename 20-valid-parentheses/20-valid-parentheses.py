379baf8b4237c964341a7ddc59b7fa096675fa42
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-07-18 15:23:08 +0900

    Time: 45 ms (12.04%), Space: 13.9 MB (5.13%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mate = {
+            '(':')',
+            '{':'}',
+            '[':']',
+        }
+        if len(s)%2 == 1:
+            return False
+        
+        stack = []
+        store = []
+        for i in range(len(s)):
+            stack.append(s[i])
+            
+        count = False
+        while stack :
+            tmp = stack.pop()
+            if tmp not in mate :
+                store.append(tmp)
+            else :
+                if not store or mate[tmp] != store.pop() :
+                    return False
+                
+        if store :
+            return False
+        return True
