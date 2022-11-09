0dc4ab4d413b7c042fab40088fe6f12bc9f5cb38
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-11-09 20:50:08 +0900

    Time: 1613 ms (38.60%), Space: 19.9 MB (59.54%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = heapq.nsmallest(k,points,key= lambda x:x[0]**2+x[1]**2)
+        
+        return result
