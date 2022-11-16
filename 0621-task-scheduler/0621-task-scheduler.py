27564f13eec679bc52ba3d02fef0e4a8889e7fe5
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-11-16 21:33:17 +0900

    Time: 1727 ms (11.25%), Space: 15.6 MB (69.38%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        counter = dict(collections.Counter(tasks))
+        can = []
+        for t in counter:
+            heapq.heappush(can,(-counter[t],t))
+
+        wait = collections.deque()
+        time = 0
+        while sum(counter.values()) != 0 :
+            if wait:
+                w = wait.popleft()
+                if w != "#":
+                    heapq.heappush(can,(-counter[w],w))
+            if can:
+                t = heapq.heappop(can)
+                counter[t[1]] -= 1
+                if counter[t[1]] != 0 :
+                    while len(wait) < n:
+                        wait.append("#")
+                    wait.append(t[1])
+            else:
+                wait.append("#")
+            time += 1
+
+        return time
