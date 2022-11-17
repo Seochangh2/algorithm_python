50db9d18ce97516fd39a95e476661b6fcf46dde6
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-11-17 18:41:48 +0900

    Time: 317 ms (38.37%), Space: 14 MB (85.89%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,24 +5,29 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        need = collections.Counter(t)
-        target = len(t)
-        left = 0
-        start = 0
-        end = 0
+        if len(t)>len(s):
+            return ""
 
-        for i,v in enumerate(s,1):
-            if need[v] > 0:
-                target -= 1
-            need[v] -= 1
+        t_len = len(t)
+        counter = collections.Counter(t)
+        
+        left = right = 0
+        start = end = 0
+        for right,v in enumerate(s,1):
+            # if right == 0:
+            #     continue
+            if counter[v]>0:
+                t_len -=1
+            counter[v] -= 1
 
-            if target == 0:
-                while left<i and need[s[left]] <0:
-                    need[s[left]] += 1
-                    left += 1
-                if not end or i - left <= end - start:
-                    start , end = left,i
-                need[s[left]] += 1
-                left+=1
-                target +=1
+            if t_len == 0:
+                while counter[s[left]]<0 and left <right :
+                    counter[s[left]] +=1
+                    left +=1
+                if not end or end-start >= right-left:
+                    end , start = right,left
+                t_len +=1
+                counter[s[left]] +=1
+                left +=1
+
         return s[start:end]
