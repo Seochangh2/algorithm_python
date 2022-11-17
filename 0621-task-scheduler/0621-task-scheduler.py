eb8d0fa16e6911e74e2a2f3474525e732a26c7d5
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-11-17 22:47:49 +0900

    Time: 2477 ms (5.15%), Space: 15.7 MB (22.64%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -6,25 +6,27 @@ class Solution(object):
         :rtype: int
         """
         counter = dict(collections.Counter(tasks))
-        can = []
-        for t in counter:
-            heapq.heappush(can,(-counter[t],t))
-
+        
         wait = collections.deque()
+        
+        remain = []
         time = 0
-        while sum(counter.values()) != 0 :
+        for t in counter:
+            heapq.heappush(remain,(-counter[t],t))
+        
+        while sum(counter.values()) != 0:
             if wait:
                 w = wait.popleft()
                 if w != "#":
-                    heapq.heappush(can,(-counter[w],w))
-            if can:
-                t = heapq.heappop(can)
-                counter[t[1]] -= 1
-                if counter[t[1]] != 0 :
-                    while len(wait) < n:
+                    heapq.heappush(remain,(-counter[w],w))
+            if remain:
+                now = heapq.heappop(remain)
+                counter[now[1]] -= 1
+                if counter[now[1]] > 0:
+                    while len(wait)<n:
                         wait.append("#")
-                    wait.append(t[1])
+                    wait.append(now[1])
 
             time += 1
-
+                    
         return time
