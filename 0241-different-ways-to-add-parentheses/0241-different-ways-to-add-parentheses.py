c131871caa1b57d618f4f41802c2834a9d28a227
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2022-11-18 16:38:59 +0900

    Time: 63 ms (21.26%), Space: 13.7 MB (34.48%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def diffWaysToCompute(self, expression):
+        """
+        :type expression: str
+        :rtype: List[int]
+        """
+        def cacul(left,right,v):
+            re = []
+            for l in left:
+                for r in right:
+                    re.append(eval(str(l)+v+str(r)))
+            return re
+        if expression.isdigit():
+            return [int(expression)]
+
+        result = []
+        for i , v in enumerate(expression):
+            if v in "+-*":
+                left = self.diffWaysToCompute(expression[:i])
+                right= self.diffWaysToCompute(expression[i+1:])
+                result.extend(cacul(left,right,v))
+        
+        return result
