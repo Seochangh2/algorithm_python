a45645be05288650eb9e107159c50cec32df75d5
changh2 <71918056+Seochangh2@users.noreply.github.com>
2023-01-12 20:14:39 +0900

    [Silver I] Title: 정수 삼각형, Time: 152 ms, Memory: 35704 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,25 +1,19 @@
-
 N = int(input())
 graph = []
 for _ in range(N):
     graph.append(list(map(int,input().split())))
-memory = [[0]*(i+1) for i in range(N)]
-memory[0][0] = graph[0][0]
-cumul = 0
-for i,floor in enumerate(graph):
-    J = len(floor)
-    if i == 0:
-        cumul += J
-    else :
-        for j in range(J):
-            if j == 0:
-                left = 0
-            else :
-                left = graph[i-1][j-1]
-            if j == (J-1):
-                right = 0
-            else :
-                right = graph[i-1][j]
-            graph[i][j] = max(left,right) + graph[i][j]
+    
+for i in range(1,N):
+    J = len(graph[i])
+    for j in range(J):
+        if j == 0:
+            left = 0
+        else:
+            left = graph[i-1][j-1]
+        if j == J-1:
+            right = 0
+        else:
+            right = graph[i-1][j]
+        graph[i][j] += max(left,right)
 
 print(max(graph[N-1]))
